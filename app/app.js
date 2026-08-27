bd3c26e31a2b3246ad74b504cbeb8c38ec4f233f
hamad933 <hamadco933@gmail.com>
2026-08-27 22:47:09 +0300

    fix(rp02): load bounded S01 contrast corrections

--- app/app.js
+++ app/app.js
@@ -12,6 +12,11 @@ import {
   simulateAction
 } from './state.js';
 
+const qualityStyles = document.createElement('link');
+qualityStyles.rel = 'stylesheet';
+qualityStyles.href = './rp02-quality-fixes.css';
+document.head.append(qualityStyles);
+
 const byId = (id) => document.getElementById(id);
 const FOCUSABLE = 'a[href],button:not([disabled]),input:not([disabled]),select:not([disabled]),textarea:not([disabled]),[tabindex]:not([tabindex="-1"])';
 
